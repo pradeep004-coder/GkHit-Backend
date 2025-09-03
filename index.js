1075162d0bea9de06b386231fa8ded6f5a748a3e
pradeep004-coder <pp0931275@gmail.com>
2025-09-03 16:45:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://gkhit.vercel.app/",
+  origin: "https://gkhit.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
