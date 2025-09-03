7f6f2f80ae5095ad41b2a58bb393ead1c4eba895
pradeep <pp0931275@gmail.com>
2025-09-03 08:53:05 +0530

    commit

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express()
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://gkhit.vercel.app/",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 mongoose.connect(process.env.mongoURL, {
     useNewUrlParser: true,
